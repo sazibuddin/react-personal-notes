58c9ac808f5ad8eda937f9ed9e1b9b1d9c0ce61f
sazibuddin <sazibuddin19@yahoo.com>
2021-01-15 04:23:51 +0600

    install redux axios

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,22 +1,48 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
 const LoginForm = () => {
-    const [email, setEmail] = useState('');
-    const [password, setpassword] = useState('');
-    return (
-        <React.Fragment>
-            <h3>Have an account ? </h3>
-            <h5>Login here</h5>
-            <br/>
-             <form noValidate>
-                <div className="form-group">
-                    <input type="email" name="email" className="form-control" placeholder="Enter your email" />
-                </div>
-            </form>
-
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
+  return (
+    <React.Fragment>
+     <div className="card">
+         <div className="card-header">
+            <h3 className="text-center">Have an account ? </h3>
+            <h5 className="text-center">Login here</h5>
+         </div>
+         <div className="card-body">
+      <form noValidate>
+        <div className="form-group">
+          <input
+            noValidate
+            type="email"
+            name="email"
+            className="form-control"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="from-group">
+          <input
+           type="password"
+           name="password"
+           className="form-control"
+           placeholder="Enter your password"
+           value={password}
+           onChange={(e) => setpassword(e.target.value)}
+            />
+        </div>
+        <div className="from-group mt-2 text-right">
+            <button type="button" className="btn btn-outline-danger">Cancel <i className="fas fa-times"></i> </button>
+            <button type="submit" className="btn btn-primary ml-2">Login <i className="fas fa-sign-in-alt"></i> </button>
             
-        </React.Fragment>
-    );
+        </div>
+      </form>
+         </div>
+     </div>
+    </React.Fragment>
+  );
 };
 
 export default LoginForm;
--- src/components/register.component.js
+++ src/components/register.component.js
@@ -1,18 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const RegisterForm = () => {
+    const [state , setState] = useState({
+        fristName:"",
+        lastName:"",
+        email:"",
+        password:""
+    });
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        const name = e.target.name;
+        setState({
+            [name]: value
+        })
+    }
 
     return (
         <>
-            <h3>New user ? </h3>
-            <h5>Create account</h5>
-            <br/>
+        <div className="card">
+            <div className="card-header">
+                <h3 className="text-center">New user ? </h3>
+                <h5 className="text-center">Create account</h5>
+            </div>
+            <div className="card-body">
+                <form noValidate>
+                    <div className="form-group">
+                        <input
+                         type="text"
+                         name="fristName"
+                         className="form-control"
+                         value={state.fristName}
+                         onChange={handleChange}
+                         />
+                    </div>
+                    <div className="form-group">
+                        <input
+                         type="text"
+                         name="lastName"
+                         className="form-control"
+                         value={state.lastName}
+                         onChange={handleChange}
+                         />
+                    </div>
+                    
+                    <div className="form-group">
+                        <input
+                         type="email"
+                         name="email"
+                         className="form-control"
+                         value={state.email}
+                         onChange={handleChange}
+                         />
+                    </div>
+                    <div className="form-group">
+                        <input
+                         type="password"
+                         name="password"
+                         className="form-control"
+                         value={state.password}
+                         onChange={handleChange}
+                         />
+                    </div>
+                    <div className="from-group mt-2 text-right">
+                        <button type="button" className="btn btn-outline-danger">Cancel <i className="fas fa-times"></i> </button>
+                        <button type="submit" className="btn btn-primary ml-2">Login <i className="fas fa-sign-in-alt"></i> </button>
+                        
+                    </div>
+                </form>
+            </div>
+        </div>
+            
 
-            {/* <form noValidate>
+            <form noValidate>
                 <div className="form-group">
 
                 </div>
-            </form> */}
+            </form>
         </>
     );
 };
